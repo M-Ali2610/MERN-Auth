21f517c5258ecd1c5c6ff56562543501c06779c9
M-Ali2610 <muhammad.ali26@live.com>
2025-01-27 19:05:04 +0500

    Added frontend url to .env, added login link to Get Started Button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,12 @@
 import { useContext } from "react"
 import { assets } from "../assets/assets"
 import { AppContent } from "../context/AppContext"
+import {useNavigate} from "react-router-dom"
 
 
 const Header = () => {
+
+    const navigate = useNavigate()
 
     const {userData} = useContext(AppContent)
   return (
@@ -19,7 +22,8 @@ const Header = () => {
         <h2 className="text-3xl sm:text-5xl font-semibold mb-4">Welcome to our app</h2>
 
         <p className="mb-8 max-w-md">Let's start with a quick product tour and we will have you up and running in no time!</p>
-        <button className="border border-gray-500 rounded-full px-8 py-2.5 hover:bg-gray-100 transition-all cursor-pointer">Get Started</button>
+        <button onClick={()=> navigate('/login')} className="border border-gray-500 rounded-full px-8 py-2.5
+         hover:bg-gray-100 transition-all cursor-pointer">Get Started</button>
     </div>
   )
 }
